250eaadd879314f95b07889378dcf9b95da1673e
sharath <sharath.savasere@gmail.com>
2018-04-07 22:51:30 +0530

    django integrated

---
+++ codefundo/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/sharath/Documents/6th-SEM/codefundo/chessboard/codefundo/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
