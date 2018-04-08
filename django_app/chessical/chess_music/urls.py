f0dd9de4e5cb64a3beef9cc85121f3995516ab48
Anishcharith <anish.charith@gmail.com>
2018-04-08 06:55:50 +0530

    forms

--- django_app/chessical/chess_music/urls.py
+++ django_app/chessical/chess_music/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name="chess_music"
 urlpatterns = [
         path('',views.index,name="index"),
+        path('',views.musicPlayer,name="musicPlayer"),
         ]
--- django_app/chessical/chess_music/views.py
+++ django_app/chessical/chess_music/views.py
@@ -5,17 +5,25 @@ import pyrebase
 
 # Create your views here.
 def index(request):
-	config = {
-	"apiKey": "AIzaSyBoFfC1va0c9vhgSbVntIWzDC-jtxVV6HI",
-	"authDomain": "chess-app-cbfb0.firebaseapp.com",
-	"databaseURL": "https://chess-app-cbfb0.firebaseio.com",
-	"storageBucket": "chess-app-cbfb0.appspot.com",
-	"projectId": "chess-app-cbfb0",
-	"messagingSenderId": "1089239373610"
-	}
-	firebase = pyrebase.initialize_app(config)
-	db=firebase.database()
-	context = {
-		"games" : list(db.get().val().items())
-	}
-	return render(request,'chess_music/index.html',context)
+    if request.method=='POST':
+        game=request.POST.get('GameList') 
+        return render(request,'chess_music/musicPlayer.html')
+
+        return HttpResponse(game) 
+    config = {
+    "apiKey": "AIzaSyBoFfC1va0c9vhgSbVntIWzDC-jtxVV6HI",
+    "authDomain": "chess-app-cbfb0.firebaseapp.com",
+    "databaseURL": "https://chess-app-cbfb0.firebaseio.com",
+    "storageBucket": "chess-app-cbfb0.appspot.com",
+    "projectId": "chess-app-cbfb0",
+    "messagingSenderId": "1089239373610"
+    }
+    firebase = pyrebase.initialize_app(config)
+    db=firebase.database()
+    context = {
+    "games" : list(db.get().val().items())
+    }
+    return render(request,'chess_music/index.html',context)
+
+def musicPlayer(request):
+    return render(request,'chess_music/musicPlayer.html')
